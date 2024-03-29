3834849c34bf023f91dd61423724c396671ad017
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-03-29 10:57:07 +0300

    not working-refactored

--- src/main.js
+++ src/main.js
@@ -8,29 +8,41 @@ import { renderImages } from "./js/render-functions.js";
 
 
 
+export let page = 1;
+
 const galleryList = document.querySelector(".gallery");
 let query;
 const input = document.querySelector("input");
 const form = document.querySelector("form");
 const loader = document.querySelector('.loader');
+const perPage = 15;
+let images;
+const fetchImagesBtn = document.querySelector(".fetch-images-btn");
+fetchImagesBtn.style.display = 'none';
 
+
+// ------ loader functions -------
 function showLoader() {
     loader.classList.remove("is-hidden");
 }
-
 function hideLoader() {
     loader.classList.add("is-hidden");
 }
 
+// ------- input event listener -----
 input.addEventListener("input", (event) => {
     event.preventDefault();
     query = input.value.trim();
-    
 });
 
-form.addEventListener("submit", (event) => {
+
+// -------form submit event listener ------
+form.addEventListener("submit", async event => {
     event.preventDefault();
+
     showLoader();
+
+    // ------- prevent empty query -------
     if (query === "") {
         iziToast.error({
             color: 'yellow',
@@ -38,21 +50,73 @@ form.addEventListener("submit", (event) => {
             position: 'topRight',
         });
     }
-    if (query) {
-        fetchImages(query)
-            .then(data => renderImages(data.hits))
-            .catch(error => {
-                console.log(error);
-                iziToast.error({
-                    title: 'Error',
-                    message: `❌ Sorry, there are no images matching your search query. Please, try again!`,
-                    position: 'topRight',
-                })
+    // -------- load images set by query --
+    try {
+        images = await fetchImages();
+
+        if (!images.length) {
+            iziToast.error({
+                title: 'Error',
+                message: `❌ Sorry, there are no images matching your search query. Please, try again!`,
+                position: 'topRight',
             })
-            .finally(() => hideLoader())
+        }
+
+        if (perPage <= images.length) {
+        fetchImagesBtn.style.display = "block";
+}
+else {
+        iziToast.error({
+            color: "blue",
+            title: 'Error',
+            message: `❌ Sorry, there are no more images to show`,
+            position: 'topRight',
+        });
     }
-    galleryList.innerHTML = "";
+        renderImages(images);   
+    } catch (error) {
+        iziToast.error({
+            title: 'Error',
+            message: `❌ Sorry, error occured!`,
+            position: 'topRight',
+        });
+    } finally {
+    hideLoader()
+    }    
 });
 
+// --------- add page ------
+
+fetchImagesBtn.addEventListener("click", async event => {
+    event.preventDefault();
+    page += 1;
+
+    showLoader()
+
+    try {
+        images = await fetchImages();
+        if (perPage > images.length) {
+            iziToast.error({
+                color: "blue",
+                title: 'Error',
+                message: `❌ Sorry, there are no more images to show`,
+                position: 'topRight',
+            });
+            fetchImagesBtn.style.display = 'none';
+            return;
+        }
+        renderImages(images);
+    } catch (error) {
+        console.log(error)
+        iziToast.error({
+            title: 'Error',
+            message: `❌ Sorry, error occured!`,
+            position: 'topRight',
+        });
+    } finally {
+        hideLoader();
+    }
+
+})
 
 
